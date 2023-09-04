9deb0025e1c0b78a1533434298bf5f9e299be3ef
Hanjunhee-1 <hanjuni857@naver.com>
2023-09-05 01:06:58 +0900

    feat: insertion sort

---
+++ sorting/insertion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void print(int* arr, int size) {
+    for (int i=0; i<size; i++) {
+        printf("%d ", arr[i]);
+    } printf("\n");
+}
+
+void insertion(int* arr, int size) {
+    int key, j;
+    for (int i=1; i<size; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j>=0 && arr[j]>key) {
+            arr[j+1] = arr[j];
+            j -= 1;
+        }
+
+        arr[j+1] = key;
+    }
+}
+
+int main() {
+    int arr[10] = {5, 3, 1, 2, 4, 10, 9, 6, 8, 7};
+    int size = sizeof(arr) / sizeof(int);
+
+    insertion(arr, size);
+    print(arr, size);
+    return 0;
+}
