80ccf5a747dc68d2f10f0647e5cb40aac6927cd1
Hanjunhee-1 <hanjuni857@naver.com>
2023-10-14 22:45:43 +0900

    insertion sort with for loop

--- sorting/insertion.c
+++ sorting/insertion.c
@@ -20,11 +20,25 @@ void insertion(int* arr, int size) {
     }
 }
 
+void insertion2(int* arr, int size) {
+    int key, j;
+    
+    for (int i=1; i<size; i++) {
+        key = arr[i];
+        
+        for (j = i - 1; j >= 0 && arr[j] > key; j--) {
+            arr[j+1] = arr[j];
+        }
+
+        arr[j+1] = key;
+    }
+}
+
 int main() {
     int arr[10] = {5, 3, 1, 2, 4, 10, 9, 6, 8, 7};
     int size = sizeof(arr) / sizeof(int);
 
-    insertion(arr, size);
+    insertion2(arr, size);
     print(arr, size);
     return 0;
 }
